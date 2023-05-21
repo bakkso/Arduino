9d2431e3df1e2c6f57ca7b2271ca1c38aade3d0c
bakkso <bbakksso@gmail.com>
2023-05-22 01:22:07 +0900

    Midterm code update

--- week8_Midterm/week8_Midterm.ino
+++ week8_Midterm/week8_Midterm.ino
@@ -85,17 +85,20 @@ void loop()
     }
 }
 
-// 오류 : inputValue 변수에 값이 들어가지 않음 && 초음파 거리측정은 가능
+// 오류1 : inputValue 변수에 값이 들어가지 않음 && 초음파 거리측정은 가능
 //       -> 스위치값은 제대로 변환은 되지만 inputValue에 변수가 들어가지 않는 이슈때문에 측정거리 비교 불가
 //       -> LED 제어 불가능
-// 해결방안1 : Serial.available에 값이 안 들어갔다는 생각
-// -> Serial.available { 괄호안에 inputValue 연산 }
-// -> 여전히 inputValue 변수안에 값이 안 들어감
-// 오류 원인 : Setup()에서 inputValue를 선언해서 지역변수가 되었다.. 큰 실수를...
-// Setup() - inputValue & loop() - inputValue 는 서로 다른 변수 loop()내에서 새롭게 지역변수로 선언됨.
-// loop()함수에서 Serial.parseInt()로 값을 읽어와서 inputValue에 저장하더라도
-// 이 값은 loop() 함수가 끝나는 즉시 사라짐.
-// 오류 : 위 오류룰 해결해도 아두이노를 업로드하고 시리얼 모니터에 값을 입력하면
+
+// 해결방안 : Serial.available에 값이 안 들어갔다는 생각
+//          -> Serial.available { 괄호안에 inputValue 연산 }
+//          -> 여전히 inputValue 변수안에 값이 안 들어감
+
+// 오류1 원인 : Setup()에서 inputValue를 선언해서 지역변수가 되었다.. 큰 실수를...
+//            Setup() - inputValue & loop() - inputValue 는 서로 다른 변수 loop()내에서 새롭게 지역변수로 선언됨.
+//            loop()함수에서 Serial.parseInt()로 값을 읽어와서 inputValue에 저장하더라도 이 값은 loop() 함수가 끝나는 즉시 사라짐.
+
+// 오류2 : 위 오류룰 해결해도 아두이노를 업로드하고 시리얼 모니터에 값을 입력하면
 //       곧이어 inputValue가 0으로 초기화되는 현상, 초음파 거리측정은 가능
-// 해결방안2 : 어차피 사용자가 숫자를 입력하는 것을 초기에 한 번 수행하고 끝이니까 setup()에서 inputValue를 설정해주면 어떨까?
+
+// 해결방안 : 어차피 사용자가 숫자를 입력하는 것을 초기에 한 번 수행하고 끝이니까 setup()에서 inputValue를 설정해주면 어떨까?
 //           -> 성공!!!!!!!!!!!! 아주 깔끔하게 수행된다.
